a6b89da3359484c8e99af833357aab5bc1832daa
Simon K <30438951+simshard@users.noreply.github.com>
2023-12-29 16:08:07 +0000

    delete confirm

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -12,7 +12,12 @@
       <tr wire:key="{{ $post->id }}" class=" bg-sky-100" >
         <td class="p-2 font-semibold">{{$post->title}}</td>
         <td class="p-2 font-light text-gray-400">{{str($post->body)->words(12)}}</td>
-        <td><button class="text-xs text-black bg-green-400 hover:bg-green-300 p-2 rounded-lg" title="delete post" type="button" wire:click="delete({{$post->id}})">Delete</button></td>
+        <td><button class="text-xs text-black bg-green-400 hover:bg-green-300 p-2 rounded-lg"
+            title="delete post"
+            type="button"
+            wire:click="delete({{$post->id}})"
+            wire:confirm  =" really delete this post?" 
+            >Delete</button></td>
       </tr>
       @endforeach
     </tbody>
