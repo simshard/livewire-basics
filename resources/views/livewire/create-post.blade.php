55159764293d4cc8a2ba4f39cd395bce3b2674e5
Simon K <30438951+simshard@users.noreply.github.com>
2023-12-30 12:22:15 +0000

    alpine attributes

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -12,8 +12,15 @@
         <div class="flex flex-col  bg-gray-100 ">
             <label for="body">Body</label>
             <textarea name="body" id="body" cols="20" rows="4" class=" bg-gray-50 mx-2 border border-sky-300 shadow-md" wire:model="body"></textarea>
-            @error('body') <span class="text-red-500">{{ $message }}</span>@enderror
+            @error('body') 
+            <span class="text-red-500">{{ $message }}</span>
+            @enderror
+            <span class="text-xs text-gray-400">Characters<span class="text-sm text-blue-500 mx-2" x-text="$wire.body.length"></span></span>
         </div>
         <button  wire:click.prevent="save" class="mt-4 bg-sky-500 rounded-lg hover:bg-sky-200 p-2 text-black">Save</button>
     </form>
+    <section class="mt-4">
+        <span class="text-xs text-gray-400"> Current Title
+        <span class="text-sm text-blue-500" x-text="$wire.title"></span> </span>
+    </section>
 </div>
